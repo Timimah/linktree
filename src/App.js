c8d6d4e7f62da8bf7731b9fb8acec364fe367e90
Timimah <44163985+Timimah@users.noreply.github.com>
2022-11-02 12:06:57 +0100

    added footer to app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 // import './App.css';
 import ProfileSec from './profileSec';
 import Links from './linkSec';
+import Footer from './footer'
 
 function App() {
   return (
     <div style={{ bacgroundColor: 'black' }}>
       <ProfileSec />
       <Links />
+      <Footer />
     </div>
   );
 }

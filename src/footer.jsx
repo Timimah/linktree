124cb6ecaffbefc20569ee8a0635306f12db6b19
Timimah <44163985+Timimah@users.noreply.github.com>
2022-11-02 12:31:05 +0100

    adjusted the footer

--- src/footer.jsx
+++ src/footer.jsx
@@ -7,11 +7,13 @@ function Footer() {
         <div>
             <hr />
             <div className="footer">
-                <img src={zuri} alt="Zuri Internship" />
-            </div>
-            <div style={{fontWeight: 100}}>Hngi9 Frontend Task</div>
-            <div>
-                <img src={I4G} alt="Ingressive for Good logo" />
+                <div className="footer_image">
+                    <img src={zuri} alt="Zuri Internship" />
+                </div>
+                <div className="footer_image" style={{fontWeight: 100}}>Hngi9 Frontend Task</div>
+                <div>
+                    <img src={I4G} alt="Ingressive for Good logo" />
+                </div>
             </div>
         </div>
     )

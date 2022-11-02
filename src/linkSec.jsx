0ef45b20da54f9695401303147a86cd6c5bca755
Timimah <44163985+Timimah@users.noreply.github.com>
2022-11-02 11:46:01 +0100

    updated links

--- src/linkSec.jsx
+++ src/linkSec.jsx
@@ -6,15 +6,31 @@ import GitHib from './images/github.png'
 function Links(){
     return (
         <div className="links">
-            <div className="link">Twitter Link</div>
-            <div className="link">Zuri Team</div>
-            <div className="link">Python Books</div>
-            <div className="link">Background Check for Coders</div>
-            <div className="link">Design Books</div>
+            <div className="link">
+                <a href="#">Twitter Link</a>
+            </div>
+            <div className="link">
+                <a href="https://book.zuri.team" id="books">Zuri Books</a>
+            </div>
+            <div className="link" id="btn_zuri">
+                <a href="https://training.zuri.team">Zuri Team</a>
+            </div>
+            <div className="link" id="book_python">
+                <a href={PythonLink()}>Python Books</a>
+            </div>
+            <div className="link" id="pitch">
+                <a href="https://background.zuri.team">Background Check for Coders</a>
+            </div>
+            <div className="link" id="book_design">Design Books</div>
             <div style={{padding: '1em'}}>
                 <img src={Slack} alt="Slack"  style={{padding: '0.2em'}} />
                 <img src={GitHib} alt="GitHub"  style={{padding: '0.2em'}} />
             </div>
         </div>
-    )}
+    )
+
+function PythonLink() {
+    return "https://books.zuri.team/python-for-beginners?ref_id=Enda-Rae";
+}
+}
 export default Links

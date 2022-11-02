3e198a3ab6b0c71d3dea85aa61151351d991e920
Timimah <44163985+Timimah@users.noreply.github.com>
2022-11-02 12:02:44 +0100

    updated footer

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import zuri from './images/zuri.png';
+import I4G from './images/I4G.png'
 
 function Footer() {
     return(
         <div>
+            <hr />
             <div>
-                <img src="" alt="Zuri Internship" />
+                <img src={zuri} alt="Zuri Internship" />
             </div>
             <div style={{fontWeight: 100}}>Hngi9 Frontend Task</div>
             <div>
-                <img src="" alt="Ingressive for Good logo" />
+                <img src={I4G} alt="Ingressive for Good logo" />
             </div>
         </div>
     )
